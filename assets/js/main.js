9e696b71ac3802fb4f92084677a665c32274d970
Rajesh <raeshprasadcoimbatore@gmail.com>
2024-03-19 19:56:03 +0530

    snack bar for send message

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,3 +265,14 @@
   });
 
 })()
+
+function showSnackBar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 10 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+}
